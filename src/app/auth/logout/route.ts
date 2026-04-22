8a4535eb192e85f7c085e96f4049f03057fc6ef4
Cristian M <140653540+tonymocanu97@users.noreply.github.com>
2026-04-22 23:40:32 +0300

    -

--- src/app/auth/logout/route.ts
+++ src/app/auth/logout/route.ts
@@ -1,7 +1,9 @@
+import { createServerSupabaseClient } from '@utils/supabase/server';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(request: NextRequest) {
+  const supabase = await createServerSupabaseClient();
+  await supabase.auth.signOut();
   const { origin } = new URL(request.url);
-
   return NextResponse.redirect(`${origin}/`);
 }
