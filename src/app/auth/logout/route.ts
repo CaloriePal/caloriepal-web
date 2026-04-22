bbe41fbde0342a4aaa4f262902355990a8a29b73
Cristian M <140653540+tonymocanu97@users.noreply.github.com>
2026-04-22 23:33:41 +0300

    -

--- src/app/auth/logout/route.ts
+++ src/app/auth/logout/route.ts
@@ -2,7 +2,6 @@ import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(request: NextRequest) {
   const { origin } = new URL(request.url);
-  const response = NextResponse.redirect(`${origin}/`);
 
-  return response;
+  return NextResponse.redirect(`${origin}/`);
 }
